5b92d0c91d88316cbf5254720522c55749cce5a0
ada elon <zjxlijo@163.com>
2023-10-16 16:30:32 +0800

    项目结果初始创建

---
+++ PeerEvaluation/src/main/java/com/zjx/courese/peerevaluation/PeerEvaluationApplication.java
@@ -0,0 +1,13 @@
+package com.zjx.courese.peerevaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerEvaluationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeerEvaluationApplication.class, args);
+	}
+
+}
---
+++ PeerEvaluation/src/test/java/com/zjx/courese/peerevaluation/PeerEvaluationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjx.courese.peerevaluation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeerEvaluationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
