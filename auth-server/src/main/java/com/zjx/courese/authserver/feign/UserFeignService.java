73ac96ec3754126107e3ba51f55ec72dc0a4c351
ada elon <zjxlijo@163.com>
2023-11-17 12:29:10 +0800

    图片上传

---
+++ auth-server/src/main/java/com/zjx/courese/authserver/feign/UserFeignService.java
@@ -0,0 +1,14 @@
+package com.zjx.courese.authserver.feign;
+
+import com.zjx.common.utils.R;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient("user")
+public interface UserFeignService {
+
+    @RequestMapping("/user/users/find/{username}")
+    public R findUserByName(@PathVariable String username);
+
+}
---
+++ user/src/main/java/com/zjx/courese/user/vo/UserLoginVo.java
@@ -0,0 +1,9 @@
+package com.zjx.courese.user.vo;
+
+import lombok.Data;
+
+@Data
+public class UserLoginVo {
+    private String name;
+    private String password;
+}
